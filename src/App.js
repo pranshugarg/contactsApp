dd6dd5ff019e27db0a3fa86f3fcdeae872e92ad1
pranshugarg <pranshugarg02@gmail.com>
2019-10-03 08:22:54 +0530

    Add CreateContact component and see state based rendering before moving to rect routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
+    screen: 'create', // list, create
     contacts: []
   }
 
@@ -12,18 +14,35 @@ class App extends Component {
       this.setState({ contacts })
     })
   }
+
   removeContact = (contact) => {
     this.setState( (state) => ({
       contacts: state.contacts.filter((c)=> c.id!== contact.id )
     }))
+
+    // Also removes from server or db from where it is fetching.(Not just UI)
+    ContactsAPI.remove(contact)
   }
 
   render() {
     return (
       <div>
-      <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+       
+       {this.state.screen === 'list' && (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+        )}
+        
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
+
       </div>
+      
     )
+    // state based content rendering
   }
 }
 
